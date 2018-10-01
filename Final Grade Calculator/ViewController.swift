df31c1696da161d3a66c63515b56d826d9ac1b5d
Emily Kirk <ekirk@emilys-macbook-air-73.local>
2018-10-01 11:23:43 -0500

    Initial Commit

---
+++ Final Grade Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Final Grade Calculator
+//
+//  Created by Emily Kirk on 10/1/18.
+//  Copyright © 2018 Emily Kirk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
