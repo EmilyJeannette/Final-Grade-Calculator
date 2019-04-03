e33553b91e2e633b1a97d8c7aeca120bd9484e01
Emily Kirk <ekirk@emilys-macbook-air-73.local>
2019-04-03 11:17:48 -0500

    Final Grade Calculator

--- Final Grade Calculator/ViewController.swift
+++ Final Grade Calculator/ViewController.swift
@@ -5,21 +5,63 @@
 //  Created by Emily Kirk on 10/1/18.
 //  Copyright © 2018 Emily Kirk. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var grade = 0.00
+    
+    @IBOutlet weak var currentGradeTextField: UITextField!
+    @IBOutlet weak var finalWeightTextField: UITextField!
+    @IBOutlet weak var desiredGradeSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var gradeNeededLabel: UILabel!
+    @IBOutlet weak var extraCreditLabel: UILabel!
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var desiredGradeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.sendSubview(toBack: backgroundView)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func onGradeChosen(_ sender: UISegmentedControl) {
+        finalWeightTextField.resignFirstResponder()
+        currentGradeTextField.resignFirstResponder()
+        
+        let operation = sender.titleForSegment(at: sender.selectedSegmentIndex)
+        switch operation
+        {
+        case "A":
+            grade = 90
+        case "B":
+            grade = 80
+        case "C":
+            grade = 70
+        default:
+            grade = 60
+        }
+        
+        if let currentGrade = Double(currentGradeTextField.text!){
+            let desiredGrade = Double(grade)
+            if let weight = Double(finalWeightTextField.text!) {
+                let gradeNeeded = (100 * desiredGrade - (100 - weight) * currentGrade) / weight
+                
+                if gradeNeeded > 100 {
+                    backgroundView.backgroundColor = UIColor.red
+                    view.backgroundColor = .red
+                }
+                else {
+                    
+                    backgroundView.backgroundColor = UIColor.green
+                    view.backgroundColor = .green
+                }
+                gradeNeededLabel.text = String(format:"%.1f", gradeNeeded) + "%"
+            }
+        }
+    }
 }
 
